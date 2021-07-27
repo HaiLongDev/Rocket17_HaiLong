26214410ca2e1dbce9f07e8bd015b7565889ed20
HaiLongDev <longdohai2828@gmail.com>
2021-07-27 22:07:47 +0700

    Assignment 1

---
+++ SQL/Assignment_1/Demo_Lesson1.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE Sale_Management;
+USE Sale_Management;
+
+CREATE TABLE CUSTOMERS (
+	customer_id 			INT,
+    first_name				VARCHAR(50),
+    last_name				VARCHAR(50),
+    email					VARCHAR(50),
+    numbers_of_complains 	INT
+);
+
+CREATE TABLE SALES (
+	purchase_number 	INT,
+    date_of_purchase 	DATE,
+    customer_id 		INT,
+    item_code			VARCHAR(50)
+);
+
+CREATE TABLE ITEMS (
+	item_code					VARCHAR(50),
+	item						VARCHAR(50),
+	unit_price_usd				INT,
+	company_id					INT,
+	company						VARCHAR(50),
+    headquarters_phone_number	VARCHAR(50)
+)
+
+	
---
+++ SQL/Assignment_1/SQL – Assignment1.sql
@@ -0,0 +1,73 @@
+CREATE TABLE SQL_Assignment1;
+USE SQL_Assignment1;
+
+CREATE TABLE Department (
+	department_id 	INT AUTO_INCREMENT,
+    department_name	VARCHAR(50)
+);
+
+CREATE TABLE Position (
+	position_id		INT AUTO_INCREMENT,
+    position_name	VARCHAR(50)
+);
+
+CREATE TABLE User_account (
+	account_id 		INT AUTO_INCREMENT,
+    email 			VARCHAR(50),
+    username 		VARCHAR(50),
+    full_name 		VARCHAR(50),
+    department_id 	INT,
+    position_id		INT,
+    create_date 	DATE
+);
+
+CREATE TABLE `Group` (
+	group_id 		INT AUTO_INCREMENT,
+    group_name		VARCHAR(50),
+    creator_id		INT,
+    create_date		DATE
+);
+
+CREATE TABLE GroupAcount(
+	group_id 	INT,
+    account_id 	INT,
+    join_date 	DATE
+);
+CREATE TABLE TypeQuestion(
+	type_id 	INT,
+    type_name 	NVARCHAR(50)
+);	
+CREATE TABLE CategoryQuestion(
+	catogory_id 	INT,
+    catogory_name 	NVARCHAR(50)
+);
+CREATE	TABLE Question(
+	question_id 	INT,
+    content 		NVARCHAR(50),
+    category_id 	INT,
+    type_id 		INT,
+    creator_id 		INT,
+    create_date 	DATE
+);
+CREATE TABLE Answer(
+	answer_id 	INT,
+    content 	NVARCHAR(50),
+    question_id INT,
+    isCorrect 	BIT
+);
+CREATE TABLE Exam(
+	exam_id 	INT,
+    code 		INT,
+    tilte 		NVARCHAR(10),
+    category_id INT,
+    duration 	TIME,
+    create_date DATE
+);
+
+CREATE TABLE Question(
+	exam_id 	INT,
+    question_id INT
+);
+    
+    
+    
